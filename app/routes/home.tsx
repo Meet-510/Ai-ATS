56fc9431b4a784ca890b99d5f20d6aa3f396cadb
Meet-510 <mistrymeet941@gmail.com>
2026-03-09 21:25:42 -0600

    API

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,28 +1,40 @@
 import NavBar from "../components/NavBar";
 import type { Route } from "./+types/home";
-import { callbackify } from "util";
+// import { callbackify } from "util";
 import { resumes } from "../constants";
 import ResumeCard from "~/components/ResumeCard";
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router";
+import { usePuterStore } from "~/lib/puter"
 
 
 export function meta({}: Route.MetaArgs) {
-  return [
+    return [
     { title: "Resumind" },
     { name: "description", content: "Smart feedback for your dream job!" },
   ];
 }
 
 export default function Home() {
+  const {auth} = usePuterStore();
+  const navigate = useNavigate();
+
+  useEffect(()=>{
+    if( ! auth.isAuthenticated) navigate('/auth?next=/');
+  },[auth.isAuthenticated])
+
+   
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
   <NavBar/>
+  
     <section className="main-section">
-      <div className="page-heading">
+      <div className="page-heading py-16">
         <h1>Track Your Application & Resume Ratings</h1>
         <h2>Review your submissions and check AI-powered feedback.</h2>
 
       </div>
 
-    </section>
+  
 
 
   {resumes.length > 0 && (
@@ -34,6 +46,7 @@ export default function Home() {
   ))}
 </div>
   )}
+    </section>
     
 
   </main>
