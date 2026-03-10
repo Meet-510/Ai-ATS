da8a04b4c1a3c32c9290774ffbbedc9c4b8ed4a0
Meet-510 <mistrymeet941@gmail.com>
2026-03-10 17:27:04 -0600

    Puter

--- app/components/FileUploader.tsx
+++ app/components/FileUploader.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {useState, useCallback} from 'react'
 import {useDropzone} from 'react-dropzone'
+import { formatSize } from '~/lib/utils';
 
 
 interface FileUploaderProps{
@@ -8,7 +9,7 @@ interface FileUploaderProps{
 }
 
 const FileUploader = ({onFileSelect}:FileUploaderProps) => {
-    const [file, setFile] = useState();
+    // const [file, setFile] = useState();
 
     const onDrop = useCallback((acceptedFiles:File[]) => {
     const file = acceptedFiles[0] || null;
@@ -35,15 +36,32 @@ const file = acceptedFiles[0] || null;
     <div className='w-full gradient-border'><div {...getRootProps()}>
       <input {...getInputProps()} />
       <div className='spce-y-4 cursor-pointer'>
-        <div className='mx-auto w-16 h-16 flex items-center justify-center'>
-            <img src='/icons/info.svg' alt='upload' className='size-20'/>
-        </div>
+        
         {file? ( 
-            <div>
+           <div className='uploader-selected-file' onClick={(e) => e.stopPropagation()}>
+              <img src="/images/pdf.png" alt= "pdf" className='size-10'/>
+            <div className='flex items-center space-x-3'>
+              
+                <div>
+                    <p className='text-sm font-medium text-gray-700 truncate max-w-xs'>
+                    {file.name}
 
+                </p>
+                <p className='text-sm text-gray-500'>
+                    {formatSize(file.size)}
+                </p>
+                </div>
+                
+            </div>
+            <button className='p-2 cursor-pointer' onClick={(e) => {onFileSelect?.(null)}}>
+                <img src='/icons/cross.svg' alt='remove' className='w-4 h-4'/>
+            </button>
             </div>
         ):(
             <div>
+                <div className='mx-auto w-16 h-16 flex items-center justify-center mb-2'>
+            <img src='/icons/info.svg' alt='upload' className='size-20'/>
+        </div>
                 <p className='text-lg text-gray-500'>
                     <span className='font-semibold'>
                         Click to upload
